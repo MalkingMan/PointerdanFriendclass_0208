137101b40412e24d5e623cb734a533071cd7dc59
MalkingMan <arrayalkhozini@gmail.com>
2024-05-07 14:48:34 +0700

    commit 1

---
+++ Pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public:
+    int panjang, lebar;
+    int luaspersegipanjang(int b, int a)
+    {
+        return a*b;
+    }
+
+
+
