160d053b56c7fe373a2014275271001397f6b96a
MalkingMan <arrayalkhozini@gmail.com>
2024-05-07 15:37:15 +0700

    commit 10

---
+++ FriendClass.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+    private:
+        int id;
+    public:
+    void setid(int pId);
+    void displayALL(orang &a);
+};
+
