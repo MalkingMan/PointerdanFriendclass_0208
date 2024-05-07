7df25e4985d7a452d6e0f62c1761eb13aa0b3219
MalkingMan <arrayalkhozini@gmail.com>
2024-05-07 15:28:06 +0700

    commit 9

--- Pointer3.cpp
+++ Pointer3.cpp
@@ -14,7 +14,11 @@ public :
 
 int main ()
 {
-    mahasiswa
+    mahasiswa *mhs = new mahasiswa(1); // pointer object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
 }
 
 
